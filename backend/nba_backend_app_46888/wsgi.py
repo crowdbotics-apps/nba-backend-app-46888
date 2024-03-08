4098b77dbc79a58b148037bdffdcf38b1581d490
Crowdbotics <team@crowdbotics.com>
2024-03-08 19:21:18 +0000

    Initial commit

---
+++ backend/nba_backend_app_46888/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for nba_backend_app_46888 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "nba_backend_app_46888.settings"
+)
+
+application = get_wsgi_application()
